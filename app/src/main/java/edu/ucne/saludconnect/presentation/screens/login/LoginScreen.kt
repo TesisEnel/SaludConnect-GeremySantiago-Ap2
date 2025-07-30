b3f727ffc2d91902d265f81a4a1e51f5a546b423
Geremy De los Santos S. <geremiorlando28@hotmail.com>
2025-07-30 17:29:55 -0400

    Se creo el composable de Login

---
+++ app/src/main/java/edu/ucne/saludconnect/presentation/screens/login/LoginScreen.kt
@@ -0,0 +1,97 @@
+package edu.ucne.saludconnect.presentation.screens.login
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import edu.ucne.saludconnect.presentation.screens.registros.registro_pacientes.InputField
+
+@Composable
+fun LoginScreen(
+    navController: NavController,
+    viewModel: LoginViewModel = hiltViewModel()
+) {
+    val state by viewModel.state
+    val context = LocalContext.current
+    val snackbarHostState = remember { SnackbarHostState() }
+
+
+    LaunchedEffect(state.mensajeExito) {
+        if (!state.mensajeExito.isNullOrEmpty()) {
+            snackbarHostState.showSnackbar(state.mensajeExito!!)
+            viewModel.limpiarMensajeExito() // 🧹 limpiar después
+        }
+    }
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(32.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Iniciar Sesión", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            InputField("Cédula", state.cedula) {
+                viewModel.onEvent(LoginEvent.CedulaChanged(it))
+            }
+
+            InputField("Teléfono", state.telefono) {
+                viewModel.onEvent(LoginEvent.TelefonoChanged(it))
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Button(
+                onClick = {
+                    viewModel.onEvent(LoginEvent.IniciarSesion(navController))
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Iniciar Sesión")
+            }
+        }
+    }
+}
+
+@Composable
+fun InputField(label: String, value: String, onValueChange: (String) -> Unit) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(label) },
+        modifier = Modifier.fillMaxWidth(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+    )
+}
