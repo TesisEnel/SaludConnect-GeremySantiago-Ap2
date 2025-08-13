1acdd2aa58adc2a2f35bcc437bb95a659407f24e
Geremy De los Santos S. <geremiorlando28@hotmail.com>
2025-08-13 04:30:23 -0400

    Se agrego cambio el nombre a la variable

--- app/src/main/java/edu/ucne/saludconnect/data/local/dao/DoctorDao.kt
+++ app/src/main/java/edu/ucne/saludconnect/data/local/dao/DoctorDao.kt
@@ -11,8 +11,8 @@ interface DoctorDao {
     @Insert(onConflict = OnConflictStrategy.ABORT)
     suspend fun insert(doctor: DoctorEntity)
 
-    @Query("SELECT * FROM doctores WHERE licencia = :licencia LIMIT 1")
-    suspend fun getByLicencia(licencia: String): DoctorEntity?
+    @Query("SELECT * FROM doctores WHERE cedula = :cedula LIMIT 1")
+    suspend fun getByCedula(cedula: String): DoctorEntity?
 
     @Query("SELECT * FROM doctores ORDER BY nombre")
     suspend fun getAll(): List<DoctorEntity>
