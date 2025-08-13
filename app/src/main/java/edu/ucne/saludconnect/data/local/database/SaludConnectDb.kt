8d6e3d8adad13081d2fc1d5fb05f9f9cf0125687
Geremy De los Santos S. <geremiorlando28@hotmail.com>
2025-08-13 17:18:03 -0400

    Se actualizo la bd

--- app/src/main/java/edu/ucne/saludconnect/data/local/database/SaludConnectDb.kt
+++ app/src/main/java/edu/ucne/saludconnect/data/local/database/SaludConnectDb.kt
@@ -2,13 +2,21 @@ package edu.ucne.saludconnect.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import edu.ucne.saludconnect.data.local.dao.DoctorDao
 import edu.ucne.saludconnect.data.local.dao.PacienteDao
+import edu.ucne.saludconnect.data.local.entity.DoctorEntity
 import edu.ucne.saludconnect.data.local.entity.PacienteEntity
 
 @Database(
-    entities = [PacienteEntity::class],
-    version = 1)
+    entities = [
+        PacienteEntity::class,
+        DoctorEntity::class],
+    version = 2,
+    exportSchema = false
+)
 
 abstract class SaludConnectDatabase : RoomDatabase() {
     abstract fun pacienteDao(): PacienteDao
+
+    abstract fun doctorDao(): DoctorDao
 }
