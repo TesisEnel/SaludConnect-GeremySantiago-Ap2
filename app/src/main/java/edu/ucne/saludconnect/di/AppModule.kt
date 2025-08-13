a3c9f6d344c20ea525c34957124e80f9789dfea7
Geremy De los Santos S. <geremiorlando28@hotmail.com>
2025-08-13 04:28:44 -0400

    Se agrego el doctor

--- app/src/main/java/edu/ucne/saludconnect/di/AppModule.kt
+++ app/src/main/java/edu/ucne/saludconnect/di/AppModule.kt
@@ -1,12 +1,12 @@
 package edu.ucne.saludconnect.di
 
-import android.content.Context
 import android.app.Application
 import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import edu.ucne.saludconnect.data.local.dao.DoctorDao
 import edu.ucne.saludconnect.data.local.dao.PacienteDao
 import edu.ucne.saludconnect.data.local.database.SaludConnectDatabase
 import javax.inject.Singleton
@@ -22,12 +22,17 @@ object AppModule {
             app,
             SaludConnectDatabase::class.java,
             "saludconnect_db"
-        ).fallbackToDestructiveMigration()
+        ).fallbackToDestructiveMigration(true)
             .build()
     }
 
     @Provides
     fun providePacienteDao(db: SaludConnectDatabase): PacienteDao {
         return db.pacienteDao()
+    }
+
+    @Provides
+    fun provideDoctorDao(db: SaludConnectDatabase): DoctorDao {
+        return db.doctorDao()
     }
 }
